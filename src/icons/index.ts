b47b0c670f436b391e6f7d71a810982c671a3635
Jesús Cedeño Vélez <jesus.27velez@gmail.com>
2026-02-28 14:58:29 -0500

    feat: add icon components

---
+++ src/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as IconHeart } from "./IconHeart.astro";
+export { default as IconPause } from "./IconPause.astro";
+export { default as IconPlay } from "./IconPlay.astro";
+export { default as IconPlayNext } from "./IconPlayNext.astro";
+export { default as IconPlayRewind } from "./IconPlayRewind.astro";
+export { default as IconShuffle } from "./IconShuffle.astro";
