a7033cb81c583f59ee6902e757f200768e3c3cba
Jesús Cedeño Vélez <jesus.27velez@gmail.com>
2026-02-28 17:15:07 -0500

    feat: add ThemeSelector component with light/dark theme icons
    Add ThemeSelector dropdown component with light and dark mode options,
    positioned absolutely in the top-right corner. Add IconMusicTheme,
    IconLightTheme, IconDarkTheme and IconTheme icons under src/icons/theme/
    with a barrel export. Integrate ThemeSelector into MainLayout.

---
+++ src/icons/theme/index.ts
@@ -0,0 +1,3 @@
+export { default as IconLightTheme } from './IconLightTheme.astro';
+export { default as IconDarkTheme } from './IconDarkTheme.astro';
+export { default as IconTheme } from './IconTheme.astro';
